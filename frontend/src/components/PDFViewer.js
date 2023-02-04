b26464465cba8e65fefec1daf116f9925a4dd23e
Sujal Singh <email.sujalsingh@gmail.com>
2023-02-04 15:29:29 +0530

    init PDFViewer

--- frontend/src/components/PDFViewer.js
+++ frontend/src/components/PDFViewer.js
@@ -1,7 +1,22 @@
-import React from "react";
+import React, {useState, useEffect, useRef} from "react";
+import {Document, Page} from 'react-pdf';
+import 'react-pdf/dist/esm/Page/TextLayer.css'
+import 'react-pdf/dist/esm/Page/AnnotationLayer.css'
+
 
 export default function PDFViewer() {
+    const [numPages, setNumPages] = useState(null);
+    const [pageNumber, setPageNumber] = useState(1);
+
+    function onDocumentLoadSuccess({numPages}) {
+        setNumPages(numPages);
+    }
+
     return (
-        <div className="pdf-container"></div>
+        <div className="pdf-container">
+            <Document file="/sample.pdf" onLoadSuccess={onDocumentLoadSuccess}>
+                <Page pageNumber={pageNumber}/>
+            </Document>
+        </div>
     )
 }
